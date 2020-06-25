ef701cbd5b31d63632ac7a3266b7a24390cf4cc2
Jonathan Deutsch <jonathan@tumult.com>
2020-06-24 17:45:04 -0700

    Fix warning: Umbrella header for module 'KSCrash' does not include header 'KSCrashMonitor_AppState.h'

--- Source/Framework/KSCrashFramework.h
+++ Source/Framework/KSCrashFramework.h
@@ -21,6 +21,7 @@
 #import "KSCrashInstallationStandard.h"
 #import "KSCrashInstallationVictory.h"
 #import "KSCrashMonitorContext.h"
+#import "KSCrashMonitor_AppState.h"
 #import "KSCrashMonitor_System.h"
 #import "KSCrashReportFilter.h"
 #import "KSCrashReportFilterAlert.h"
